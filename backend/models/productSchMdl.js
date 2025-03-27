124f90d44770895dd0c6163c42982fa6142fc6bd
kriskris-27 <krishnakumars0101@gmail.com>
2025-03-27 09:00:19 +0530

    product Route completed

---
+++ backend/models/productSchMdl.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");//import mongo db
+
+
+//schema
+//the structure or object
+
+const productSchema = mongoose.Schema({name:{type:String,required:true,},
+price:{type:Number,required:true},
+stock:{type:Number,required:true},
+category:{type:String,required:true},
+expiryDate:{type:Date}},
+{timestamps:true});
+
+
+//model
+//(why model ??? (To interact with this schema in MongoDB, we need to use mongoose.model()))
+
+
+const Product = mongoose.model("Product",productSchema);
+
+
+
+//make it accessible for all
+
+module.exports =Product;
