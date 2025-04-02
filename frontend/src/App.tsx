ca8a084ef9fbbaeebbc0d13f048bbb265ab14e11
kriskris-27 <krishnakumars0101@gmail.com>
2025-04-02 14:31:51 +0530

    frontend file setup completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+        Tailwind is working! 🎉
+      </div>
+    );
+  }
+  
