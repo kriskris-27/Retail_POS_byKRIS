933583e9fa959a2df9fd8e54001b8aab80eafbe1
Kris <krishnakumars0101@gmail.com>
2025-04-27 00:52:25 +0530

    test -1

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,13 +1,13 @@
 const express= require("express")
-const { adminAuth } = require("../middleware/adminAuthMiddleware");
+const { adminOnly, protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 const { getUsers, createUser, updateUser, deleteUser } = require("../controllers/adminCont");
 
 
-router.get('/',adminAuth,getUsers);
-router.post('/create',adminAuth,createUser);
-router.put('/update/:id',adminAuth,updateUser);
-router.delete('/delete/:id',adminAuth,deleteUser);
+router.get('/',protect,adminOnly,getUsers);
+router.post('/create',protect,adminOnly,createUser);
+router.put('/update/:id',protect,adminOnly,updateUser);
+router.delete('/delete/:id',protect,adminOnly,deleteUser);
 
 module.exports= router
 
