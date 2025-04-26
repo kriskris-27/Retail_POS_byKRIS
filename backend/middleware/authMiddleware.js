e785cbc37e918feec23c29082a49820a33a40c3b
Kris <krishnakumars0101@gmail.com>
2025-04-27 03:35:30 +0530

    final

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,9 +4,9 @@ const protect = (req, res, next) => {
     const token = req.cookies.token;
     console.log("Cookies received:", req.cookies);
 
-    if (!token) return res.status(401).json({ message: "No token, authorization denied" });
   
     try {
+        
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = decoded;
       next();
