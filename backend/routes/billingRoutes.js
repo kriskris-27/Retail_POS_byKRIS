961abecb64f7303a6743346ec724ab57edb4f8d3
Kris <krishnakumars0101@gmail.com>
2025-04-27 03:35:30 +0530

    final

--- backend/routes/billingRoutes.js
+++ backend/routes/billingRoutes.js
@@ -3,13 +3,13 @@ const { protect, cashierOrAdmin,adminOnly } =require("../middleware/authMiddlewa
 const { saveInvoice, generatePDF, fetchBill ,deleteBills} = require("../controllers/billingCont");
 const router= express.Router();
 
-
-router.post("/create", saveInvoice);
-router.get("/", fetchBill);
+// Apply protect middleware to all routes
+router.post("/create", protect,cashierOrAdmin,saveInvoice);
+router.get("/", protect,fetchBill);
 
 router.get("/invoice/:billId",generatePDF);
 
-router.delete("/",deleteBills);
+router.delete("/", protect,adminOnly,deleteBills);
 
 
 module.exports=router;
